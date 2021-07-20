aab23e28bd9b0b3d0958730b488d489fc2c307e7
RamsesDelta <ramses.garcia.cabrera@ibm.com>
2021-07-20 18:18:54 -0500

    add sigin function

--- src/components/auth/RegisterSreen.js
+++ src/components/auth/RegisterSreen.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm'
+import validator from 'validator'
+import {useDispatch, useSelector} from 'react-redux'
+import { removeError, setError } from '../actions/ui'
+import { startRegisterWithPasswordName } from '../actions/auth'
 
 export const RegisterSreen = () => {
+
+    const dispatch = useDispatch()
+
+    const {msgError} = useSelector(state => state.ui)
 
     const [formValues, handleInputChange ] = useForm({
         name:'Hernando',
@@ -17,7 +25,7 @@ export const RegisterSreen = () => {
         e.preventDefault()
 
         if(isFormValid()){
-            console.log('formulario correcto')
+           dispatch(startRegisterWithPasswordName(email,password,name))
         }
         
     }
@@ -25,10 +33,16 @@ export const RegisterSreen = () => {
     const isFormValid = () =>{
 
         if(name.trim().length === 0){
-            console.log('name is requiered')
+            dispatch(setError('name is requiered'))
+            return false
+        }else if(!validator.isEmail(email)){
+            dispatch(setError('invalid email'))
+            return false
+        }else if(password !== password2 || password.length <=5){
+            dispatch(setError('error en las contraseñas'))
             return false
         }
-
+        dispatch(removeError())
         return true
     }
 
@@ -38,7 +52,7 @@ export const RegisterSreen = () => {
             
             <form onSubmit={handleRegister}>
                 
-                <div className="auth__alert-error"> Hola mundo </div>
+               { msgError && (<div className="auth__alert-error"> {msgError}</div>)}
 
                 <input className="auth__input" type="text" placeholder="Name" name="name" autoComplete="off" value={name} onChange={handleInputChange}/>
                 <input className="auth__input" type="text" placeholder="Email" name="email" autoComplete="off" value={email} onChange={handleInputChange}/>
